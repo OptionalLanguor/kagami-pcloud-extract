aee62a3893d1497dec401b92366bc1164f7c52f4
OptionalLanguor <felmt@hotmail.com>
2018-08-22 19:00:23 -0300

    Build with CMake.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-g++ -o kagami-pcloud.exe src/main.cpp
+# kagami-pc-cloud-extract
+
+## build
+
+´´´shell
+cmake .
+make
+
+´´´
+
+## run
+
+´´´shell
+./kagami-pcloud-extract.exe simple-dataset/ground-truth/0000000410.png
+
+´´´
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,33 @@
 #include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <cstdio>
 
 using namespace cv;
 
-int main(){
+int main (int argc, char** argv) {
 
- printf("Hello!\n");
+	printf("\nkagami-pcloud-extract\n\n");
 
- return 0;
+	if (argc != 2)
+ 	{
+		printf("Send as argument an image.\n");
+		return -1;
+ 	}
+	printf("Image sent as argument: \"%s\"\n", argv[1]);
+
+	Mat image;
+
+	image = imread(argv[1], CV_LOAD_IMAGE_ANYDEPTH);
+
+	if ( !image.data )
+  {
+		printf("No image data \n");
+    return -1;
+	}
+
+	namedWindow("Display Original Image", WINDOW_AUTOSIZE );
+	imshow("Display Original Image", image);
+
+ 	return 0;
 }
